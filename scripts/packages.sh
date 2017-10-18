9a3a29c3bcfb1b79222ef23844b51ede7a27dfa0
Daniel D <djx339@gmail.com>
2017-10-18 01:51:14 -0500

    Update packages.sh

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -1,7 +1,7 @@
 apt-get clean
 apt-get update
 apt-get upgrade -y
-apt-get install -y linux-generic linux-headers-generic linux-headers-server linux-image-generic linux-server
+apt-get install -y linux-generic linux-headers-generic linux-headers-server linux-image-generic linux-server acl
 
 # Reboot with the new kernel so we can build the proper guest additions
 shutdown -r now
